491d251f8faf6899da6d1cf1a873df1ec757580f
Wendy Wang <wangwenusa11@gmail.com>
2017-07-01 00:11:57 -0700

    Added action

--- popup.js
+++ popup.js
@@ -1,5 +1,20 @@
 $(function() {
-	$('#name').keyup(function() {
-		$('#greeting').text('Hi, ' + $('#name').val() + '!');
+	$('#addAmount').click(function() {
+		chrome.storage.sync.get('total', function(items){
+			var newTotal = 0;
+			if (items.total) {
+				newTotal += parseInt(items.total);
+			}
+
+			var amount = $('amount').val();
+			if (amount) {
+				newTotal += parseInt(amount);
+			}
+
+			chrome.storage.sync.set({'total': newTotal});
+
+			$('total').text(newTotal);
+			$('#amount').val('');
+		})
 	})
 });
