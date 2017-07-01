68a2f501d1b714b61c75ba0b87845abe751c3d4a
Wendy Wang <wangwenusa11@gmail.com>
2017-07-01 13:01:26 -0700

    Added time is not working

--- popup.js
+++ popup.js
@@ -2,17 +2,18 @@ $(function() {
 	chrome.storage.sync.get(['total', 'goal'], function(items) {
 		$('#total').text(items.total);
 		$('#goal').text(items.goal);
-	})
+	});
+
 	$('#addAmount').click(function() {
 		chrome.storage.sync.get(['total', 'goal'], function(items){
 			var newTotal = 0;
 			if (items.total) {
-				newTotal += parseInt(items.total);
+				newTotal = newTotal + parseInt(items.total);
 			}
 
 			var amount = $('#amount').val();
 			if (amount) {
-				newTotal += parseInt(amount);
+				newTotal = newTotal +  parseInt(amount);
 			}
 
 			chrome.storage.sync.set({'#total': newTotal});
@@ -27,9 +28,8 @@ $(function() {
 					message: "You've been slacking for " + items.goal + "!",
 					iconUrl: "icon.png"
 				}
+				chrome.notifications.create('goalReached', opt, function(){ });
 			}
-
-			chrome.notifications.create('goalReached', opt, function(){ });
-		})
-	})
+		});
+	});
 });
